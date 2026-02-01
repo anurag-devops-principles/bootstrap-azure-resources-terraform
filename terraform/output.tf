244a1c8e510e37ff42c500f6829d2fc5e7c2aa0c
Anurag Vijay <anurag_vijay@Anurags-MacBook-Air.local>
2026-02-01 10:52:03 +0530

    feat: terraform directory added

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,6 +8,11 @@ output "storage_account_name" {
   value       = azurerm_storage_account.bootstrap.name
 }
 
+output "storage_container_name" {
+  description = "The name of the storage account."
+  value       = azurerm_storage_container.bootstrap.name
+}
+
 output "key_vault_name" {
   description = "The name of the Key Vault."
   value       = azurerm_key_vault.bootstrap.name
